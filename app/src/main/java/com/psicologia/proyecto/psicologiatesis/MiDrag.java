e37d1e38aae857b6fcc8e2cac364ced3dbc81b58
henry jaramillo <henri0720@hotmail.com>
2015-07-24 21:53:19 -0500

    movimiento todas las imagenes y cambio a transpa
    cambiar nombres quedan con mayusculas creo

---
+++ app/src/main/java/com/psicologia/proyecto/psicologiatesis/MiDrag.java
@@ -0,0 +1,36 @@
+package com.psicologia.proyecto.psicologiatesis;
+
+import android.view.DragEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+/**
+ * Created by Henry Jaramillo on 24/07/2015.
+ */
+final class MiDrag implements View.OnDragListener{
+
+
+    public boolean onDrag(View contenedor_nuevo, DragEvent evento){
+        switch (evento.getAction()){
+            case DragEvent.ACTION_DRAG_ENTERED:
+                break;
+            case DragEvent.ACTION_DRAG_EXITED:
+                break;
+            case DragEvent.ACTION_DROP:
+                View objeto = (View) evento.getLocalState();
+                ViewGroup papa_objeto = (ViewGroup) objeto.getParent();
+                papa_objeto.removeView(objeto);
+
+                LinearLayout contenedor_nuevo2 = (LinearLayout) contenedor_nuevo;
+                contenedor_nuevo2.addView(objeto);
+
+                objeto.setVisibility(View.VISIBLE);
+                break;
+            default:
+                break;
+        }
+        return true;
+    }
+
+}
--- app/src/main/java/com/psicologia/proyecto/psicologiatesis/atencionActivity.java
+++ app/src/main/java/com/psicologia/proyecto/psicologiatesis/atencionActivity.java
@@ -1,7 +1,9 @@
 package com.psicologia.proyecto.psicologiatesis;
 
+import android.annotation.TargetApi;
 import android.content.ClipData;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
@@ -24,9 +26,47 @@ public class atencionActivity extends ActionBarActivity implements View.OnTouchL
             if(Vatencion.equals("1")){
                 setContentView(R.layout.token_test);
                 findViewById(R.id.img_circulogradeazul).setOnTouchListener(this);
+                findViewById(R.id.img_circulogranderojo).setOnTouchListener(this);
+                findViewById(R.id.img_circulograndeamarillo).setOnTouchListener(this);
+                findViewById(R.id.img_circulograndeblanco).setOnTouchListener(this);
+                findViewById(R.id.img_circulograndeverde).setOnTouchListener(this);
+                findViewById(R.id.img_circulopequenioblanco).setOnTouchListener(this);
+                findViewById(R.id.img_circulopequenioazul).setOnTouchListener(this);
+                findViewById(R.id.img_circulopequenioamarillo).setOnTouchListener(this);
+                findViewById(R.id.img_circulopequeniorojo).setOnTouchListener(this);
+                findViewById(R.id.img_circulopequenioverde).setOnTouchListener(this);
+                findViewById(R.id.img_cuadradograndeazul).setOnTouchListener(this);
+                findViewById(R.id.img_cuadradogranderojo).setOnTouchListener(this);
+                findViewById(R.id.img_cuadradograndeblanco).setOnTouchListener(this);
+                findViewById(R.id.img_cuadradograndeverde).setOnTouchListener(this);
+                findViewById(R.id.img_cuadradograndeamarillo).setOnTouchListener(this);
+                findViewById(R.id.img_cuadradopequenioamarillo).setOnTouchListener(this);
+                findViewById(R.id.img_cuadradopequenioverde).setOnTouchListener(this);
+                findViewById(R.id.img_cuadradopequeniorojo).setOnTouchListener(this);
+                findViewById(R.id.img_cuadradopequenioazul).setOnTouchListener(this);
+                findViewById(R.id.img_cuadradopequenioblanco).setOnTouchListener(this);
                 View circulograndeazul = findViewById(R.id.img_circulogradeazul);
+                View circulogranderojo = findViewById(R.id.img_circulogranderojo);
+                View circulograndeamarillo = findViewById(R.id.img_circulograndeamarillo);
+                View circulograndeblanco = findViewById(R.id.img_circulograndeblanco);
+                View circulograndeverde = findViewById(R.id.img_circulograndeverde);
+                View circulopequenioblanco = findViewById(R.id.img_circulopequenioblanco);
+                View circulopequenioazul = findViewById(R.id.img_circulopequenioazul);
+                View circulopequenioamarillo = findViewById(R.id.img_circulopequenioamarillo);
+                View circulopequeniorojo = findViewById(R.id.img_circulopequeniorojo);
+                View circulopequenioverde = findViewById(R.id.img_circulopequenioverde);
+                View cuadradograndeazul = findViewById(R.id.img_cuadradograndeazul);
+                View cuadradogranderojo = findViewById(R.id.img_cuadradogranderojo);
+                View cuadradograndeblanco = findViewById(R.id.img_cuadradograndeblanco);
+                View cuadradograndeverde = findViewById(R.id.img_cuadradograndeverde);
+                View cuadradograndeamarillo = findViewById(R.id.img_cuadradograndeamarillo);
+                View cuadradopequenioamarillo = findViewById(R.id.img_cuadradopequenioamarillo);
+                View cuadradopequenioverde = findViewById(R.id.img_cuadradopequenioverde);
+                View cuadradopequeniorojo = findViewById(R.id.img_cuadradopequeniorojo);
+                View cuadradopequenioazul = findViewById(R.id.img_cuadradopequenioazul);
+                View cuadradopequenioblanco = findViewById(R.id.img_cuadradopequenioblanco);
+                circulograndeamarillo.setOnDragListener(new MiDrag());
             }
-
         }
     }
 
@@ -42,15 +82,17 @@ public class atencionActivity extends ActionBarActivity implements View.OnTouchL
 
     @Override
     public boolean onTouch(View objeto, MotionEvent evento) {
+        boolean e;
+        e= false;
         if(evento.getAction() == MotionEvent.ACTION_DOWN){
-            ClipData datos = ClipData.newPlainText("","");;
+            ClipData datos = ClipData.newPlainText("","");
             View.DragShadowBuilder objeto_sombra = new View.DragShadowBuilder(objeto);
             objeto.startDrag(datos, objeto_sombra, objeto, 0);
             objeto.setVisibility(View.INVISIBLE);
-            return true;
+            e=true;
         }
-        else{
-            return false;
-        }
+        return e;
     }
 }
+
+
