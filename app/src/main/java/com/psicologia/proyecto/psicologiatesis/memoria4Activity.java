a0ac725840381ed2e2f71689d5c0724b8c7de63a
henry jaramillo <henri0720@hotmail.com>
2015-07-27 21:41:20 -0500

    memoria hasta resultado

---
+++ app/src/main/java/com/psicologia/proyecto/psicologiatesis/memoria4Activity.java
@@ -0,0 +1,84 @@
+package com.psicologia.proyecto.psicologiatesis;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by Henry Jaramillo on 27/07/2015.
+ */
+
+public class memoria4Activity extends ActionBarActivity {
+    String Vfunciones;
+    String Vlenguaje;
+    String Vatencion;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Bundle b = this.getIntent().getExtras();
+        super.onCreate(savedInstanceState);
+        if (b != null) {
+            Vfunciones = b.getString("VFunciones");
+            Vlenguaje = b.getString("VLenguaje");
+            Vatencion = b.getString("VAtencion");
+
+            setContentView(R.layout.memoria4);
+            final CounterClass timer = new CounterClass(8000, 1000);
+            timer.start();
+        }
+
+    }
+    public void enviarDatos(){
+        Intent data = new Intent(this,memoriaResultadoActivity.class);
+        data.putExtra("VFunciones",Vfunciones);
+        data.putExtra("VLenguaje",Vlenguaje);
+        data.putExtra("VAtencion",Vatencion);
+        startActivity(data);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected (MenuItem item){
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu (Menu menu){
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    public class CounterClass extends CountDownTimer {
+
+        public CounterClass(long millisInFuture, long countDownInterval) {
+            super(millisInFuture, countDownInterval);
+            // TODO Auto-generated constructor stub
+        }
+
+
+        @Override
+        public void onTick(long millisUntilFinished) {
+            // TODO Auto-generated method stub
+
+            long millis = millisUntilFinished;
+            String hms = String.format("%02d:%02d:%02d", TimeUnit.MILLISECONDS.toHours(millis),
+                    TimeUnit.MILLISECONDS.toMinutes(millis) - TimeUnit.HOURS.toMinutes(TimeUnit.MILLISECONDS.toHours(millis)),
+                    TimeUnit.MILLISECONDS.toSeconds(millis) - TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(millis)));
+            System.out.println(hms);
+
+        }
+
+
+
+
+        @Override
+        public void onFinish() {
+            // TODO Auto-generated method stub
+            enviarDatos();
+        }
+
+    }
+}
---
+++ app/src/main/java/com/psicologia/proyecto/psicologiatesis/memoriaResultadoActivity.java
@@ -0,0 +1,15 @@
+package com.psicologia.proyecto.psicologiatesis;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+/**
+ * Created by Henry Jaramillo on 27/07/2015.
+ */
+public class memoriaResultadoActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
