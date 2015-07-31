971bd45309a50a6dba57416876ef0f984a8c0cd1
henry jaramillo <henri0720@hotmail.com>
2015-07-30 22:26:45 -0500

    si queires haces commit no?

--- app/src/main/java/com/psicologia/proyecto/psicologiatesis/VisualActivity.java
+++ app/src/main/java/com/psicologia/proyecto/psicologiatesis/VisualActivity.java
@@ -2,11 +2,14 @@ package com.psicologia.proyecto.psicologiatesis;
 
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by Henry Jaramillo on 27/07/2015.
@@ -24,6 +27,13 @@ public class VisualActivity extends ActionBarActivity implements DialogInterface
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.visual_a);
+
+
+        final CounterClass timer = new CounterClass(180000, 1000);
+        timer.start();
+
+
+
         final TextView cuatro_uno= (TextView) findViewById(R.id.lb_4_1);
             cuatro_uno.setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -1188,6 +1198,41 @@ public class VisualActivity extends ActionBarActivity implements DialogInterface
 
     @Override
     public void onClick(DialogInterface dialog, int which) {
+
+    }
+
+
+
+    public class CounterClass extends CountDownTimer {
+
+        public CounterClass(long millisInFuture, long countDownInterval) {
+            super(millisInFuture, countDownInterval);
+            // TODO Auto-generated constructor stub
+        }
+
+
+        @Override
+        public void onTick(long millisUntilFinished) {
+            // TODO Auto-generated method stub
+
+            long millis = millisUntilFinished;
+            String hms = String.format("%02d:%02d:%02d", TimeUnit.MILLISECONDS.toHours(millis),
+                    TimeUnit.MILLISECONDS.toMinutes(millis) - TimeUnit.HOURS.toMinutes(TimeUnit.MILLISECONDS.toHours(millis)),
+                    TimeUnit.MILLISECONDS.toSeconds(millis) - TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(millis)));
+            System.out.println(hms);
+
+        }
+
+
+
+
+        @Override
+        public void onFinish() {
+            // TODO Auto-generated method stub
+
+        }
+
+
 
     }
 }
