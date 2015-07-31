e4d1eda3c1b284365b3bcfd2c5d5b6e5e95b832e
henry jaramillo <henri0720@hotmail.com>
2015-07-30 20:33:10 -0500

    4_1

--- app/src/main/java/com/psicologia/proyecto/psicologiatesis/VisualActivity.java
+++ app/src/main/java/com/psicologia/proyecto/psicologiatesis/VisualActivity.java
@@ -1,5 +1,6 @@
 package com.psicologia.proyecto.psicologiatesis;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
@@ -10,16 +11,28 @@ import android.widget.TextView;
 /**
  * Created by Henry Jaramillo on 27/07/2015.
  */
-public class VisualActivity extends ActionBarActivity {
+public class VisualActivity extends ActionBarActivity implements DialogInterface.OnClickListener {
+
 
     int contadorA = 0;
     int comision=0;
-    TextView validar;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.visual_a);
+        final TextView cuatro_uno= (TextView) findViewById(R.id.lb_4_1);
+            cuatro_uno.setOnClickListener(new View.OnClickListener(){
+
+
+                @Override
+                public void onClick(View v) {
+                    labelOnClick(cuatro_uno);
+                }
+            });
+
     }
 
     @Override
@@ -36,7 +49,6 @@ public class VisualActivity extends ActionBarActivity {
 
     public void labelOnClick(TextView id){
 
-        System.out.println(id.getText().toString());
         if(id.getText().toString().equalsIgnoreCase("lb_5_1")||id.getText().toString().equalsIgnoreCase("lb_7_2")||id.getText().toString().equalsIgnoreCase("lb_1_3")||id.getText().toString().equalsIgnoreCase("lb_8_4")||id.getText().toString().equalsIgnoreCase("lb_3_6")||id.getText().toString().equalsIgnoreCase("lb_7_8")||id.getText().toString().equalsIgnoreCase("lb_1_11")||id.getText().toString().equalsIgnoreCase("lb_3_12")||id.getText().toString().equalsIgnoreCase("lb_4_13")||id.getText().toString().equalsIgnoreCase("lb_3_15")||id.getText().toString().equalsIgnoreCase("lb_2_16")||id.getText().toString().equalsIgnoreCase("lb_7_16")||id.getText().toString().equalsIgnoreCase("lb_4_17")||id.getText().toString().equalsIgnoreCase("lb_7_19")||id.getText().toString().equalsIgnoreCase("lb_1_19")||id.getText().toString().equalsIgnoreCase("lb_5_8")){
             contadorA++;
         }
@@ -45,6 +57,12 @@ public class VisualActivity extends ActionBarActivity {
         }
         id.setText("");
 
+
     }
 
+
+    @Override
+    public void onClick(DialogInterface dialog, int which) {
+
+    }
 }
