d760cb347f39a6b945712b78e06220693562c8d8
henry jaramillo <henri0720@hotmail.com>
2015-08-02 21:17:53 -0500

    Pista audio corregida y reproducida

--- app/src/main/java/com/psicologia/proyecto/psicologiatesis/lenguajeActivity.java
+++ app/src/main/java/com/psicologia/proyecto/psicologiatesis/lenguajeActivity.java
@@ -3,15 +3,21 @@ package com.psicologia.proyecto.psicologiatesis;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  * Created by jairo on 23/07/2015.
  */
 public class lenguajeActivity extends ActionBarActivity {
+
+    String tiempo;
+
     @Override
     public void finish() {
         Intent data = new Intent(this,UsuarioActivity.class);
@@ -44,9 +50,17 @@ public class lenguajeActivity extends ActionBarActivity {
     }
 
     public void playOnClick(View v){
-        MediaPlayer mp = MediaPlayer.create(this, R.raw.prueba);
+        MediaPlayer mp = MediaPlayer.create(this, R.raw.prueba_auditiva_a_mezcla);
         mp.start();
+        final CounterClass timer = new CounterClass(161000, 1000);
+        timer.start();
     }
+
+    public void botonOnClick(View v){
+
+    }
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -57,4 +71,41 @@ public class lenguajeActivity extends ActionBarActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         return super.onOptionsItemSelected(item);
     }
+
+    public class CounterClass extends CountDownTimer {
+
+        public CounterClass(long millisInFuture, long countDownInterval) {
+            super(millisInFuture, countDownInterval);
+            // TODO Auto-generated constructor stub
+        }
+
+
+        @Override
+        public void onTick(long millisUntilFinished) {
+            // TODO Auto-generated method stub
+
+            long millis = millisUntilFinished;
+            String hms = String.format("%02d:%02d:%02d", TimeUnit.MILLISECONDS.toHours(millis),
+                    TimeUnit.MILLISECONDS.toMinutes(millis) - TimeUnit.HOURS.toMinutes(TimeUnit.MILLISECONDS.toHours(millis)),
+                    TimeUnit.MILLISECONDS.toSeconds(millis) - TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(millis)));
+            System.out.println(hms);
+            tiempo=hms;
+
+        }
+
+
+
+
+        @Override
+        public void onFinish() {
+            // TODO Auto-generated method stub
+
+        }
+
+
+
+    }
+
+
+
 }
